99e4b25df0914a2476dc7e5cbe900a4a1410c10f
Nicolas Martinez <niicomartiinez22@gmail.com>
2022-09-26 07:39:54 -0300

    Notebook

---
+++ 14-Eventos/js/02-scripts.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector('.navegacion');
+
+// Registrar un evento
+nav.addEventListener('click', () => {
+    
+})
+
+/*
+click
+dblclick = doble clicl
+mousedown - similar al click
+mouseup - cuando sueltas el click
+*/
---
+++ 14-Eventos/js/07-scripts.js
@@ -0,0 +1,14 @@
+const cardDiv = document.querySelector('.card');
+
+cardDiv.addEventListener('click', e => {
+
+    if(e.target.classList.contains('card')) {
+        console.log('Diste click en el card');
+    }
+    if(e.target.classList.contains('titulo')) {
+        console.log('Diste click en el titulo');
+    }
+    if(e.target.classList.contains('precio')) {
+        console.log('Diste click en el precio');
+    }
+});
