cd27b5a01e4b4661957d849c483cd402c6c6ee2e
Nicolas Martinez <niicomartiinez22@gmail.com>
2022-10-14 10:37:15 -0300

    Actualización de curso

---
+++ 17-PROYECTO-Buscador/js/app.js
@@ -0,0 +1,52 @@
+// Variables
+const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
+const max = new Date().getFullYear();
+const min = max - 10;
+
+// Generar un objeto con la búsqueda
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: '',
+}
+
+// Eventos
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarAutos();
+
+    // Muestra los automóviles al cargar
+
+    // Llena las opciones de años
+    llenarSelect();
+})
+
+
+// Funciones
+function mostrarAutos() {
+    autos.forEach( auto => {
+        const { marca, modelo, year, puertas, transmision, precio, color } = auto;
+        const autoHTML = document.createElement('p');
+
+        autoHTML.textContent = `
+            ${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmisión: ${transmision} - Precio: $${precio} - Color: ${color}
+        `
+        // Insertar en el HTML
+        resultado.appendChild(autoHTML);
+    })
+}
+
+// Genera los años del select
+function llenarSelect() {
+    
+    for( let i = max; i >= min; i--) {
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent  = i;
+        year.appendChild(opcion) // Agrega las opciones de años al select
+    }
+}
